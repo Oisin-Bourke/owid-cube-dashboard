6171d37230a6beadb0ade2ad98f2f3ba79b25e43
Oisin Bourke <37476725+Oisin-Bourke@users.noreply.github.com>
2026-01-28 19:27:39 +0000

    added work in progress banner

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,9 +2,18 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import ListingsPage from "./pages/listings/page"
 import DetailPage from "./pages/details/page"
 
+const WipBanner = () => {
+	return (
+		<div className='border-b bg-yellow-50 px-4 py-2 text-sm text-yellow-900'>
+			🚧 This project is a work in progress. Data and features may change.
+		</div>
+	)
+}
+
 const App = () => {
 	return (
 		<BrowserRouter>
+			<WipBanner />
 			<Routes>
 				<Route path='/' element={<ListingsPage />} />
 				<Route path='/country/:isoCode' element={<DetailPage />} />
